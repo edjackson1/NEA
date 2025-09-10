2e8a6b8e56adbd5835b79e059d9dd18f7b80b563
edjackson1 <edjackson100808@gmail.com>
2025-09-10 18:15:44 +0100

    character movement

---
+++ New Unity Project/Assets/Scripts/Lightbandit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightbandit : MonoBehaviour {
+	//declare a new atribute of the data type 'Animator'
+	Animator MyAnimator;
+
+
+	// Use this for initialization
+	//connect attribute with our characters animator
+	void Start() {
+		MyAnimator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		//when key D is pressed, run will be set to true
+			if (Input.GetKey(KeyCode.W) || 
+				Input.GetKey(KeyCode.A) ||
+				Input.GetKey(KeyCode.S) ||
+				Input.GetKey(KeyCode.D)) 
+		{
+			MyAnimator.SetBool("run", true);
+		}
+		else
+		{
+			MyAnimator.SetBool("run", false);
+		}
+
+		
+
+		
+	}
+}
