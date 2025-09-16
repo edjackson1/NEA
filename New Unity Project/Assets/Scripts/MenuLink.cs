c705da20257c6daaec0a8bc0f2a1e130c3c7e8e0
edjackson1 <edjackson100808@gmail.com>
2025-09-16 23:26:54 +0100

    Menu Partly finished

---
+++ New Unity Project/Assets/Scripts/MenuLink.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLink : MonoBehaviour
+{
+
+	public void PlayGame()
+	{
+		SceneManager.LoadSceneAsync("Game 1");
+	}
+
+	public void MenuGame()
+	{
+		SceneManager.LoadSceneAsync("Menu");
+	}
+
+	public void SettingsGame()
+	{
+		SceneManager.LoadSceneAsync("Settings");
+	}
+	
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	public void PauseGame()
+	{
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			SceneManager.LoadSceneAsync("Menu");
+		}
+	}
+
+}
+
+
