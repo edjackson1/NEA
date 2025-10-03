3391d1fec81b4c555c88f042a44cfd8d2c1d563f
edjackson1 <edjackson100808@gmail.com>
2025-10-03 15:05:35 +0100

    3/10 adding cmments

--- New Unity Project/Assets/Scripts/Lightbanditmovement.cs
+++ New Unity Project/Assets/Scripts/Lightbanditmovement.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class Lightbanditmovement : MonoBehaviour
 {
 
-	public float movSpeed = 5f;
-	Rigidbody2D rb;
+	public float movSpeed = 5f; //sets the variable of movSpeed to the value of 5
+	Rigidbody2D rb; //setting the rigidbody 2d to a variable to make it easier to use in my code
 	float speedx, speedy;
 	// Use this for initialization
-	private SpriteRenderer spriteRenderer;
+	private SpriteRenderer spriteRenderer; //declares a private variable called SpriteRenderer
 
 	void Start()
 	{
-		rb = GetComponent<Rigidbody2D>();
-		spriteRenderer = GetComponent<SpriteRenderer>();
+		rb = GetComponent<Rigidbody2D>(); //allows my sprite to access and manipulate the way physics will act on the sprite. For example gravity
+		spriteRenderer = GetComponent<SpriteRenderer>();//renders the sprite asset on screen using the position, rotation and scale
 
 
 	}
@@ -22,26 +22,33 @@ public class Lightbanditmovement : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate()
 	{
-		float direction = Input.GetAxisRaw("Horizontal");
+		float direction = Input.GetAxisRaw("Horizontal"); //retrieves the value of the horizontal axis and then stores it as a float
 
-		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))//checks to see if the user has inputed WASD and if it has it will initiate what is in the below
 		{
-			speedx = Input.GetAxisRaw("Horizontal") * movSpeed;
-			speedy = Input.GetAxisRaw("Vertical") * movSpeed;
-			rb.velocity = new Vector2(speedx, speedy);
+			speedx = Input.GetAxisRaw("Horizontal") * movSpeed; //calculates the horizontal speed from the WASD input
+			speedy = Input.GetAxisRaw("Vertical") * movSpeed; //calculates the vertical speed from the WASD input
+			rb.velocity = new Vector2(speedx, speedy); //sets the velocity of a 2D object using the speedx and speedy
 		}
 		else
 		{
-			rb.velocity = new Vector2(0, 0);
+				rb.velocity = new Vector2(0, 0); //sets the velocity to 0,0 when it isnt moving( 0 speed, 0 direction)
 		}
+		if (direction > 0) // if the user is pressing D as the vector will be greater than 0 
 
-		if (direction > 0)
-		{
-			spriteRenderer.flipX = true;
-		}
-		else if (direction < 0)
-		{
-			spriteRenderer.flipX = false;
-		}
+		{ 
+
+			spriteRenderer.flipX = true; //change the way the sprite is facing 
+
+		} 
+
+		else if (direction < 0) //if the user is pressing A as the vector will be less than 0  
+
+		{ 
+
+			spriteRenderer.flipX = false; //change the way the sprite is moving 
+
+		} 
 	}
 }
