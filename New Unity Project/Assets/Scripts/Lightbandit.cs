5de0dedc0bb133c58041f15c411a93c1f6eea589
edjackson1 <edjackson100808@gmail.com>
2025-10-14 20:57:24 +0100

    sprint 4 , enemies start of health bars for enemies

--- New Unity Project/Assets/Scripts/Lightbandit.cs
+++ New Unity Project/Assets/Scripts/Lightbandit.cs
@@ -31,27 +31,30 @@ public class Lightbandit : MonoBehaviour
 		{
 			MyAnimator.SetBool("run", false);
 		}
-		
+			
 		if(Input.GetKey(KeyCode.Mouse0)) //when left click is pressed
 		{
-			MyAnimator.SetBool("attack", true); //play the attack animation 
-		}
-		else
-		{
-			MyAnimator.SetBool("attack", false); //don't play attack animation 
+			MyAnimator.Play("attack"); //play the attack animation 
 		}
 
 	}
-
-	private void OnCollisionEnter2D(Collision2D collision) // creating a new void for when my sprite collides with the object
+		private Enemy enemyComponent;
+	 	private void OnCollisionEnter2D(Collision2D collision)//creating a new void for when my sprite collides with the object
+		{
+			enemyComponent = collision.gameObject.GetComponent<Enemy>();//checking if the enemy has a script
+		} 
+	
+	
+	
+		public void DamageEnemy() //so it can be used on the animation timeline
 	{
-		Enemy enemyComponent = collision.gameObject.GetComponent<Enemy>(); //it retrieves the script from the Enemy script
-		if(enemyComponent != null) //if the enemy component is not equal to null
-		{
-			enemyComponent.TakeDamage(1); // the enemy will take 1 damage
-		}
+				if(enemyComponent != null) //if the enemy component is not equal to null
+					{
+						enemyComponent.TakeDamage(1); // the enemy will take 1 damage
+						Debug.Log("Enemy took damage"); //will show a debug message if the enemy is taking damage
+						enemyComponent = null; //stops the enemy from taking damage more than once
+					}
 	}
-	
-	
-
 }
+
+
--- New Unity Project/Assets/Scripts/MenuLink.cs
+++ New Unity Project/Assets/Scripts/MenuLink.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuLink : MonoBehaviour
 {
-
 	public void PlayGame()
 	{
 		SceneManager.LoadSceneAsync("Game 1");
