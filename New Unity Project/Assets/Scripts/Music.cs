8bc1a051fe084a02b46085c0881e2770090bbd76
edjackson1 <edjackson100808@gmail.com>
2025-11-11 21:59:54 +0000

    music finished

--- New Unity Project/Assets/Scripts/Music.cs
+++ New Unity Project/Assets/Scripts/Music.cs
@@ -2,15 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Music : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+public class Music : MonoBehaviour
+{
+void Awake() 
+	{
+	DontDestroyOnLoad(this); //don't destroy the music when switching between scenes.
 	}
 }
