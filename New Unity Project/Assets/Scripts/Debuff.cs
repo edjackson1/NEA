78c3d4678219282b350b0f725b7af0fcf6e2cfe6
edjackson1 <edjackson100808@gmail.com>
2026-01-05 10:08:36 +0000

    sprint 7

---
+++ New Unity Project/Assets/Scripts/Debuff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debuff : MonoBehaviour {
+public Lightbanditmovement LBM;
+	// Use this for initialization
+
+
+	private void OnTriggerEnter2D(Collider2D collision) //will cause a trigger when colliding with the debuff
+	{
+		if (collision.CompareTag ("Player")) { //if the gameObject with the tag "Player collides"
+			LBM.Speed(-2); //decrease players movement speed by 2
+			Debug.Log ("Player's speed decreased"); //creates a debug to let me know if the players movement speed is being decreased
+			Destroy (gameObject); //will destroy the debuff so it can not be used more than once.
+
+
+		}
+	}
+}
--- New Unity Project/Assets/Scripts/Lightbanditmovement.cs
+++ New Unity Project/Assets/Scripts/Lightbanditmovement.cs
@@ -23,26 +23,24 @@ public class Lightbanditmovement : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate()
 	{
-		float direction = Input.GetAxisRaw("Horizontal");
+		float direction = Input.GetAxisRaw ("Horizontal");
 
-		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
-		{
-			speedx = Input.GetAxisRaw("Horizontal") * movSpeed;
-			speedy = Input.GetAxisRaw("Vertical") * movSpeed;
-			rb.velocity = new Vector2(speedx, speedy);
-		}
-		else
-		{
-			rb.velocity = new Vector2(0, 0);
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.D)) {
+			speedx = Input.GetAxisRaw ("Horizontal") * movSpeed;
+			speedy = Input.GetAxisRaw ("Vertical") * movSpeed;
+			rb.velocity = new Vector2 (speedx, speedy);
+		} else {
+			rb.velocity = new Vector2 (0, 0);
 		}
 
-		if (direction > 0)
-		{
+		if (direction > 0) {
 			spriteRenderer.flipX = true;
-		}
-		else if (direction < 0)
-		{
+		} else if (direction < 0) {
 			spriteRenderer.flipX = false;
 		}
 	}
-}
+		public void Speed(int speedAmount) //making new function to change the speed of my lightbandit
+		{
+			movSpeed += speedAmount; //will change the players speed by the speedAmount 
+		} 
+	}
